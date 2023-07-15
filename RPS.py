93d3939049b2ba2ffc18bb8a9f4cc263ed65f08b
Siddharth Madhavan <79624807+HuntingHack@users.noreply.github.com>
2023-07-15 09:12:05 +0300

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -9,61 +9,47 @@ p_cmd = input('rock: r,paper: p,scissor: s ; ')
 
 
 
-def fun():
-    ai_s = 0
-    p_s = 0
+ai_s = 0
+p_s = 0
 
-    if ai_s <= 3 or p_s <= 3:
+if ai_s <= 3 or p_s <= 3:
 
-        #if same cmds
+  #if same cmds
 
-        if ai_cmd == 'p' and p_cmd == 'p':
-            return 
+  if ai_cmd == 'p' and p_cmd == 'p':
+    return 
 
-        if ai_cmd == 'r' and p_cmd == 'r':
-            return
+  if ai_cmd == 'r' and p_cmd == 'r':
+    return
 
-        if ai_cmd == 's' and p_cmd == 's':
-            return
+  if ai_cmd == 's' and p_cmd == 's':
+    return
 
-        #if player wins
+  #if player wins
 
-        if ai_cmd == 'p' and p_cmd == 's':
-            p_s = p_s+1
-            print('You win!!')
+  if ai_cmd == 'p' and p_cmd == 's':
+    p_s = p_s+1
+    print('You win!!')
 
-        if ai_cmd == 'r' and p_cmd == 'p':
-            p_s = p_s+1    
-            print('You win!!')
+  if ai_cmd == 'r' and p_cmd == 'p':
+     p_s = p_s+1    
+     print('You win!!')
 
-        if ai_cmd == 's' and p_cmd == 'r':
-            p_s = p_s+1
-            print('You win!!')
-
-
-        #if AI wins
-
-        if ai_cmd == 's' and p_cmd == 'p':
-            ai_s = ai_s+1
-            print('AI wins!!,better luck next time!')
-
-        if ai_cmd == 'p' and p_cmd == 'r':
-            ai_s = ai_s+1    
-            print('AI wins!!,better luck next time!')
-
-        if ai_cmd == 'r' and p_cmd == 's':
-            ai_s = ai_s+1    
-            print('AI wins!!,better luck next time!')
+ if ai_cmd == 's' and p_cmd == 'r':
+      p_s = p_s+1
+      print('You win!!')
 
 
-    
-fun()
+  #if AI wins
 
+  if ai_cmd == 's' and p_cmd == 'p':
+    ai_s = ai_s+1
+    print('AI wins!!,better luck next time!')
 
-        
+  if ai_cmd == 'p' and p_cmd == 'r':
+     ai_s = ai_s+1    
+     print('AI wins!!,better luck next time!')
 
- 
-
-
-
-
+  if ai_cmd == 'r' and p_cmd == 's':
+     ai_s = ai_s+1    
+     print('AI wins!!,better luck next time!')
